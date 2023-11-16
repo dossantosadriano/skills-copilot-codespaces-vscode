856487a820964a7b061dadf58dc7a533b09e8b22
dossantosadriano <151073887+dossantosadriano@users.noreply.github.com>
2023-11-16 20:08:58 +0000

    fix third commit

--- comments.js
+++ comments.js
@@ -1,29 +1,52 @@
-// create web server with express
+// Create web server
+// Create a port
+// Import the comments file
+// Import the body-parser
+// Import the cors
+// Use the body-parser
+// Use the cors
+// Get all comments
+// Get a comment by id
+// Post a comment
+// Update a comment
+// Delete a comment
+// Listen to port
+// Path: comments.js
+// create comments
+const comments = [
+    { id: 1, name: 'John', comment: 'Hello World' },
+    { id: 2, name: 'Brad', comment: 'Hi' },
+    { id: 3, name: 'Mary', comment: 'Good Morning' }
+];
+// export comments
+module.exports = comments;
+// Path: index.js
+// Create web server
 const express = require('express');
 const app = express();
-// create a port
+// Create a port
 const port = 3000;
-// import the comments file
+// Import the comments file
 const comments = require('./comments');
-// import the body-parser
+// Import the body-parser
 const bodyParser = require('body-parser');
-// import the cors
+// Import the cors
 const cors = require('cors');
-// use the body-parser
+// Use the body-parser
 app.use(bodyParser.json());
-// use the cors
+// Use the cors
 app.use(cors());
-// get all comments
+// Get all comments
 app.get('/comments', (req, res) => {
     res.json(comments);
 });
-// get a comment by id
+// Get a comment by id
 app.get('/comments/:id', (req, res) => {
-    const comment = comments.find(comment => comment.id === parseInt(req.params.id));
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
     if (!comment) res.status(404).send('The comment with the given ID was not found.');
     res.send(comment);
 });
-// post a comment
+// Post a comment
 app.post('/comments', (req, res) => {
     const comment = {
         id: comments.length + 1,
@@ -33,21 +56,49 @@ app.post('/comments', (req, res) => {
     comments.push(comment);
     res.send(comment);
 });
-// update a comment
+// Update a comment
 app.put('/comments/:id', (req, res) => {
-    const comment = comments.find(comment => comment.id === parseInt(req.params.id));
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
     if (!comment) res.status(404).send('The comment with the given ID was not found.');
     comment.name = req.body.name;
     comment.comment = req.body.comment;
     res.send(comment);
 });
-// delete a comment
+// Delete a comment
 app.delete('/comments/:id', (req, res) => {
-    const comment = comments.find(comment => comment.id === parseInt(req.params.id));
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
     if (!comment) res.status(404).send('The comment with the given ID was not found.');
     const index = comments.indexOf(comment);
     comments.splice(index, 1);
     res.send(comment);
 });
-// listen to port
+// Listen to port
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+// Path: index.js
+// Create web server
+// Create a port
+// Import the comments file
+// Import the body-parser
+// Import the cors
+// Use the body-parser
+// Use the cors
+// Get all comments
+// Get a comment by id
+// Post a comment
+// Update a comment
+// Delete a comment
+// Listen to port
+// Path: index.js
+// Create web server
+// Create a port
+// Import the comments file
+// Import the body-parser
+// Import the cors
+// Use the body-parser
+// Use the cors
+// Get all comments
+// Get a comment by id
+// Post a comment
+// Update a comment
+// Delete a comment
+// Listen to port
